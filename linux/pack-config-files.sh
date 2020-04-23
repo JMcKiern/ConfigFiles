d4f7b88c97c37bbafb853c42370177ca4907cd92
jmckiern <jmckiern@tcd.ie>
2020-04-23 14:28:07 +0100

    Reorganise dir structure and add linux scripts

---
+++ linux/pack-config-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+files=()
+for filename in $(find ./files -type f); do
+	rel_filename=$(realpath --relative-to=files $filename)
+	files+=($rel_filename)
+	echo Found $rel_filename
+done
+
+for filename in "${files[@]}"; do
+	echo cp ./files/$filename ~/$filename
+	cp ~/$filename ./files/$filename
+done
