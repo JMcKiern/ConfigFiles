3403c995a0ef812243a3bc5cd04fdb5696dbb60f
jmckiern <jmckiern@tcd.ie>
2020-06-01 15:45:41 +0100

    Fix message

--- linux/pack-config-files.sh
+++ linux/pack-config-files.sh
@@ -3,6 +3,6 @@
 dotfile_dir="${BASH_SOURCE%/*}/files"
 for filename in $(find $dotfile_dir -type f); do
 	rel_filename=$(realpath --relative-to=$dotfile_dir $filename)
-	echo coping $rel_filename to home dir
+	echo coping $rel_filename from home dir
 	cp ~/$rel_filename $dotfile_dir/$rel_filename
 done
