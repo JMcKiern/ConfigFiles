7c866c1a1a7a9ff084b2aed992ffb9fcdd10fad7
jmckiern <jmckiern@tcd.ie>
2020-06-14 10:24:50 +0100

    bar: Add player and small refactor

--- files/.i3/status.sh
+++ files/.i3/status.sh
@@ -2,6 +2,12 @@
 
 interval=5
 
+function track {
+	title=$(playerctl metadata title)
+	artist=$(playerctl metadata artist)
+	echo "$artist - $title"
+}
+
 function volume {
 	echo $(pulsemixer --get-volume | awk '{ printf "Vol: %s%%", $1 }')
 }
@@ -23,20 +29,41 @@ function disk {
 }
 
 function gpu {
-	gpu_use=$(nvidia-settings -t -q GPUUtilization | sed -n 's/graphics=\([0-9]*\),.*/\1/p')
-	vram_use=$(nvidia-settings -t -q GPUUtilization | sed -n 's/.*memory=\([0-9]*\),.*/\1/p')
+ 	output=$(nvidia-settings -q GPUUtilization)
+ 	gpu_use=$(echo $output | sed -n 's/^.*graphics=\([0-9]*\),.*$/\1/p')
 
 	used=$(nvidia-settings -t -q UsedDedicatedGPUMemory)
 	total=$(nvidia-settings -t -q TotalDedicatedGPUMemory)
-	vram_pct=$(echo "($used * 1.0 / $total) * 100" | bc -l)
-
+	vram_pct=$(echo "$used * 100 / $total" | bc)
 
 	printf "GPU (VRAM): %s%% (%i%%)" $gpu_use $vram_pct
 }
 
+# function gpu {
+# 	output=$(nvidia-settings -q GPUUtilization -q UsedDedicatedGPUMemory -q TotalDedicatedGPUMemory)
+# 	gpu_use=$(echo $output | sed -n 's/^.*graphics=\([0-9]*\),.*$/\1/p')
+#  	# vram_use=$(echo $output | sed -n 's/^.*memory=\([0-9]*\),.*$/\1/p')
+
+#  	used=$(echo $output | sed -n "s/^.*Attribute 'UsedDedicatedGPUMemory'.*): \([0-9]*\)\..*$/\1/p")
+#  	total=$(echo $output | sed -n "s/^.*Attribute 'TotalDedicatedGPUMemory'.*): \([0-9]*\)\..*$/\1/p")
+# 	vram_pct=$(echo "($used * 100) / $total" | bc)
+
+# 	printf "GPU (VRAM): %s%% (%s%%)" $gpu_use $vram_pct
+# }
+
 function cpu {
 	cpu_use=$(mpstat | grep all | awk '{ printf "CPU: %s%%", $3 }')
 	echo $cpu_use
+}
+
+function mem {
+	mem=$(free -m | grep -m1 Mem: | awk '{ printf "Mem: %1.1f/%1.1f GiB", $3/1000, $2/1000 }')
+	echo $mem
+}
+
+function get_date {
+	currdate=$(date +'%Y-%m-%d %H:%M:%S')
+	echo $currdate
 }
 
 function sync {
@@ -49,8 +76,20 @@ function sync {
 
 while true
 do
-	mem=$(free -m | grep -m1 Mem: | awk '{ printf "Mem: %1.1f/%1.1f GiB", $3/1000, $2/1000 }')
-	date=$(date +'%Y-%m-%d %H:%M:%S')
-	echo $(volume) \| $(cpu) \| $(gpu) \| $mem \| $(disk) \| $date
+	bar=("$(track)"
+		"$(volume)"
+		"$(cpu)"
+		"$(gpu)"
+		"$(mem)"
+		"$(disk)"
+		"$(get_date)"
+	)
+	out=""
+	for ((i = 0; i < ${#bar[@]}; i++))
+	do
+		out+="| ${bar[$i]} "
+	done
+	out=${out:1}
+	echo $out
 	sync
 done
